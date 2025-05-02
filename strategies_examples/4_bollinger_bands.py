df4347b445b2dce0753c564f440a511e31ab91da
Nazarii Protskiv <rosaka1429@gmail.com>
2025-05-02 10:21:03 +0300

    update 4th strategy

--- strategies_examples/4_bollinger_bands.py
+++ strategies_examples/4_bollinger_bands.py
@@ -3,15 +3,15 @@ from typing import List
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-from indicators import add_bollinger_bands
 from utils.add_trades_marks import add_trades_marks_
 from utils.config import LONG, SHORT
-from utils.utils import load_csv
+from utils.indicators import add_bollinger_bands
 from utils.stats import Stats
 from utils.trade import Trade
+from utils.utils import load_csv
 
 
-def simulate_macd_atr_strategy(data):
+def simulate_bbands_strategy(data):
     trades: List[Trade] = []
     position = None
     trade_id = 1
@@ -142,5 +142,5 @@ def plot_trades(data, trades):
 
 
 if __name__ == '__main__':
-    df = load_csv("../test_data/custom_csvs/AAVE_USDT_RECENT.csv")
-    simulate_macd_atr_strategy(df)
+    df = load_csv("../custom_test_data/custom_csvs/AAVE_USDT_RECENT.csv")
+    simulate_bbands_strategy(df)
